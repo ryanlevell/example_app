625c6f1689494286a06e57f1317233abb974576c
ryanlevell <ryanlevell@users.noreply.github.com>
2023-02-01 22:44:05 -0600

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,18 @@
+
+def appVerion
+def codeRevision
+
 pipeline {
     agent 'any'
     stages {
         stage('Validate Commit') {
             steps {
-                build 'CD/1_commit_stage'
+                script {
+                  codeRevision = checkout(scm).GIT_COMMIT
+                  echo "Code revision: $codeRevision"
+                    
+                  build 'CD/1_commit_stage'
+                }
             }
         }
         stage('Deploy To Prod-like Env') {
@@ -17,7 +26,7 @@ pipeline {
             steps {
                 build job: 'CD/3_acceptance_test_stage', parameters: [
                     string(name: 'app_version', value: '1.0.0'),
-                    string(name: 'code_revision', value: '34RG456')
+                    string(name: 'code_revision', value: codeRevision)
                 ]
             }
         }
