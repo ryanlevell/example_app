beb3f6502abb29498cc9330949205dcf089ff8a3
Ryan L <ryanlevell@users.noreply.github.com>
2023-02-01 22:56:40 -0600

    1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,8 +8,9 @@ pipeline {
         stage('Validate Commit') {
             steps {
                 script {
-                  codeRevision = checkout(scm).GIT_COMMIT
-                  echo "Code revision: $codeRevision"
+                  appVersion = sh(returnStdOut: true, script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout').trim()
+                  codeRevision = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+                  echo "App version: $appVersion, Code revision: $codeRevision"
                     
                   build 'CD/1_commit_stage'
                 }
@@ -18,14 +19,14 @@ pipeline {
         stage('Deploy To Prod-like Env') {
             steps {
                 build job: 'CD/2_deploy_prod_like_environment', parameters: [
-                    string(name: 'app_version', value: '1.0.0')
+                    string(name: 'app_version', value: appVersion)
                 ]
             }
         }
         stage('Run Acceptance Tests') {
             steps {
                 build job: 'CD/3_acceptance_test_stage', parameters: [
-                    string(name: 'app_version', value: '1.0.0'),
+                    string(name: 'app_version', value: appVersion),
                     string(name: 'code_revision', value: codeRevision)
                 ]
             }
