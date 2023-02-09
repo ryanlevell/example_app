fe0725f8296059e7f017f24bfe028e122a6aba84
Ryan L <ryanlevell@users.noreply.github.com>
2023-02-08 21:07:39 -0600

    Added browser test

--- acceptance_test/src/test/groovy/TestSpec.groovy
+++ acceptance_test/src/test/groovy/TestSpec.groovy
@@ -1,9 +1,20 @@
 import spock.lang.Specification
 
+import static com.codeborne.selenide.Selenide.$
+import static com.codeborne.selenide.Selenide.open
+
 class TestSpec extends Specification {
 
     def "a test spec"() {
         expect:
         2 + 2 == 4
+    }
+
+    def "a browser test"() {
+        when:
+        open("https://spring-moon-2764.fly.dev/dashboard/home")
+
+        then:
+        $("h1").text() == "Hello thymeleaf!"
     }
 }
